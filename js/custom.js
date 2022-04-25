92d665cd58f6537b0e204a05c567fe46644cae92
Ashraf0231 <ashraf.anas.hoque@gmail.com>
2022-04-25 15:24:57 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".slick-sl").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 500,
+    infinite: true,
+    cssEase: "ease-in-out",
+    loop: true,
+    dots: true,
+    arrows: false,
+  });
+});
